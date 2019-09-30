14e0c69a11833740217c483902681f1dcaf3a9ca
Jeffersontads <jeffersontads@hotmail.com>
2019-09-30 20:06:28 -0300

    atualizado o arquivo pedido no front

--- pyzza/static/pyzza/customizerjs.js
+++ pyzza/static/pyzza/customizerjs.js
@@ -3,3 +3,12 @@ $('.btn-expand-collapse').click(function(e) {
 				$('.navbar-primary').toggleClass('collapsed');
 });
 /*END MENU LATERAL*/
+
+/*MEU ARRAY DE PIZZAS*/
+var arrayPizza = new Array("calabresa","peperoni","mussarela","tomate-seco");
+
+var options = ["1", "2", "3", "4", "5"];
+$('#pizzasEscolha').empty();
+$.each(options, function(i, p) {
+    $('#pizzasEscolha').append($('<select></select>').val(p).html(p));
+});
