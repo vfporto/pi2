277f11734fbcfcdc5cc9df96a104731685c5eff2
Jeffersontads <jeffersontads@hotmail.com>
2019-09-30 19:22:35 -0300

    primeiro commit front

--- pyzza/urls.py
+++ pyzza/urls.py
@@ -14,9 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+
+from pyzza import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('pyzza.urls'))
+    path('', views.home, name='home'),
+    path('pedido', views.pedido, name='pedido'),
+    path('login', views.login, name='login'),
 ]
--- pyzza/views.py
+++ pyzza/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+def home(request):
+ return render(request, 'pyzza/home.html')
+
+
+def login(request):
+ return render(request, 'pyzza/login.html')
+
+
+def pedido(request):
+ return render(request, 'pyzza/pedido.html')
