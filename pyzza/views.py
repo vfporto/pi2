5e9b736ff30c918c437146e012ca58c0ce59a257
vfporto <2643007+vfporto@users.noreply.github.com>
2019-10-21 03:20:21 -0300

    testes com serializers

--- pyzza/views.py
+++ pyzza/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from pyzza.models import SaborPizza, TipoPizza, Bebida, BebidaTamanhoBebida, ItemBebida, TamanhoBebida
+from pyzza.models import SaborPizza, TipoPizza, Bebida, BebidaTamanhoBebida, ItemBebida, TamanhoBebida, Ingrediente, \
+    SaborPizzaIngrediente
 from rest_framework import serializers,viewsets
 from rest_framework.filters import SearchFilter
 
@@ -29,6 +30,17 @@ def cardapio(request):
  return render(request, 'pyzza/cardapio.html')
 
 
+class Ingrediente_Serializer(serializers.ModelSerializer):
+    class Meta:
+        model = Ingrediente
+        fields = ('__all__')
+
+class SaborPizzaIngrediente_Serializer(serializers.ModelSerializer):
+    ingrediente = Ingrediente_Serializer()
+    class Meta:
+        model = SaborPizzaIngrediente
+        fields = ('__all__')
+
 #classe de serializacao que transforma um model em JSON
 class TipoPizza_Serializer(serializers.ModelSerializer):
     class Meta:
@@ -38,29 +50,44 @@ class TipoPizza_Serializer(serializers.ModelSerializer):
 
 class Pizza_Serializer(serializers.ModelSerializer):
     tipo_pizza = TipoPizza_Serializer()
+    ingredientes = SaborPizzaIngrediente_Serializer(many=True, read_only=True)
     class Meta:
         model = SaborPizza
         # fields = ('__all__')
-        fields = ('id', 'nome', 'descricao', 'valor_adicional', 'imagem','tipo_pizza')
+        fields = ('id', 'nome', 'descricao', 'valor_adicional', 'imagem','tipo_pizza', 'ingredientes')
+
+
+
 
 # nao to conseguindo serializar igual a pizza
+class TamanhoBebida_Serializer(serializers.ModelSerializer):
+    class Meta:
+        model = TamanhoBebida
+        fields = ('__all__')
+
+
 class BebidaTamanhoBebida_Serializer(serializers.ModelSerializer):
+    tamanho_bebida = TamanhoBebida_Serializer()
     class Meta:
         model = BebidaTamanhoBebida
-        fields = ('preco', 'bebida', 'tamanho_bebida')
-
+        # fields = ('preco', 'bebida', 'tamanho_bebida')
+        fields = ('__all__')
 
 class Bebida_Serializer(serializers.ModelSerializer):
-    #tamanho_bebida = BebidaTamanhoBebida_Serializer()
+    tamanhos = BebidaTamanhoBebida_Serializer(many=True, read_only=True)
     class Meta:
         model = Bebida
         fields = ('__all__')
         # fields = ('preco', 'bebida', 'tamanho_bebida')
 
 
+
+# ViewSets
+
+
 class Pizza_Viewset (viewsets.ModelViewSet):
-    # queryset = SaborPizza.objects.filter(disponivel=True)
-    queryset = SaborPizza.objects.all()
+    queryset = SaborPizza.objects.filter(disponivel=True)
+    # queryset = SaborPizza.objects.all()
     # queryset = queryset.prefetch_related()
     serializer_class = Pizza_Serializer
 
