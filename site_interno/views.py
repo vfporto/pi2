5074a15ac068e482ca81f2d24b47a9ba533cbbf6
Jeffersontads <Probook4530$>
2019-12-01 17:46:19 -0300

    fechou dashboard e relatorios

--- site_interno/views.py
+++ site_interno/views.py
@@ -1,8 +1,9 @@
 from django.contrib.auth.decorators import login_required
 from django.db.models import Prefetch
-from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, reverse
 
-from pedidos.models import Pedido
+from pedidos.models import Pedido, StatusPedido, ItemPizza, ItemBebida
 from pessoas.models import Entregador
 from pizzas.models import Ingrediente
 
@@ -19,13 +20,37 @@ def rel_ingredientes(request):
 
 @login_required(login_url='/admin/login')
 def rel_entregadores(request):
-    lista = Entregador.objects.filter(pedido__status_pedido=3).distinct().prefetch_related(Prefetch('pedido', Pedido.objects.filter(status_pedido=3)))
+    lista = Entregador.objects.filter(pedido__status_pedido_id__gte=3, pedido__status_pedido_id__lte=4).distinct().prefetch_related(Prefetch('pedido', Pedido.objects.filter(status_pedido_id__gte=3, status_pedido_id__lte=4)))
     # lista = Entregador.objects.all()
     return render(request, 'site_interno/rel_entregadores.html', {'lista': lista})
 
 @login_required(login_url='/admin/login')
 def dashboard_pedidos(request):
     # TODO: corrigir a busca de pedidos do dashboard
-    lista = Pedido.objects.filter(status_pedido_id__lt=3)
-    return render(request, 'site_interno/dashboard_pedidos.html', {'lista': lista})
+    lista = Pedido.objects.filter(status_pedido_id__lt=4)
+    entregadores = Entregador.objects.all()
+    return render(request, 'site_interno/dashboard_pedidos.html', {'lista': lista, 'entregadores': entregadores})
 
+
+@login_required(login_url='/admin/login')
+def set_status_pedido(request, pedido_id, status_id):
+    p = Pedido.objects.get(pk=pedido_id)
+    p.status_pedido = StatusPedido.objects.get(pk=status_id)
+    p.save()
+
+    return HttpResponseRedirect('/interno/dashboard_pedidos/',request)
+
+
+@login_required(login_url='/admin/login')
+def set_status_pedido_em_entrega(request, pedido_id):
+    entregador=request.POST['entregador']
+    Pedido.objects.filter(pk=pedido_id).update(status_pedido=3, entregador=entregador)
+    return HttpResponseRedirect('/interno/dashboard_pedidos/',request)
+
+@login_required(login_url='/admin/login')
+def rel_pizzas_produzidas(request):
+    listapizzas = ItemPizza.objects.all()
+    listabebidas = ItemBebida.objects.all()
+
+    # entregador = Entregador.objects.all()
+    return render(request, 'site_interno/rel_pizzas_produzidas.html', {'listapizzas': listapizzas, 'listabebidas': listabebidas})
